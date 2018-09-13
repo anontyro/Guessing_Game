414d69f519d2c9700e22879cd9866cacafc2055e
Alexander Wilkinson <alexwilkinson@gmail.com>
2018-09-13 21:51:26 +0800

    brought in the random number crate

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,15 @@
+extern crate rand;
+
 use std::io;
+use rand::Rng;
 
 fn main() {
     println!("Guess the number");
+
+    let number = rand::thread_rng().gen_range(1,101);
+
+    println!("Random nunber: {}",number );
+
     let mut guess = String::new();
 
     io::stdin().read_line(&mut guess)
